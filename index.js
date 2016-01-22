ea808b16a37013254c15381295d06d9219ffd8a1
snollygolly <snollygolly@gmail.com>
2016-01-22 13:58:24 -0600

    finished linting

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const config = require('./config.json');
+const config = require("./config.json");
 
-const koa = require('koa');
-const hbs = require('koa-hbs');
-const serve = require('koa-static-folder')
+const koa = require("koa");
+const hbs = require("koa-hbs");
+const serve = require("koa-static-folder");
 
 // for passport support
-const session = require('koa-generic-session');
-const bodyParser = require('koa-bodyparser');
-const passport = require('koa-passport');
+const session = require("koa-generic-session");
+const bodyParser = require("koa-bodyparser");
+const passport = require("koa-passport");
 
 const app = koa();
 
@@ -17,10 +17,10 @@ exports.app = app;
 exports.passport = passport;
 
 // the auth model for passport support
-require('./models/auth');
+require("./models/auth");
 
 // misc handlebars helpers
-require('./helpers/handlebars');
+require("./helpers/handlebars");
 
 // trust proxy
 app.proxy = true;
@@ -37,38 +37,38 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // statically serve assets
-app.use(serve('./assets'));
+app.use(serve("./assets"));
 
 // load up the handlebars middlewear
 app.use(hbs.middleware({
-  viewPath: __dirname + '/views',
-  layoutsPath: __dirname + '/views/layouts',
-  partialsPath: __dirname + '/views/partials',
-  defaultLayout: 'main'
+	viewPath: `${__dirname}/views`,
+	layoutsPath: `${__dirname}/views/layouts`,
+	partialsPath: `${__dirname}/views/partials`,
+	defaultLayout: "main"
 }));
 
-app.use(function *(next) {
-  try {
-    yield next;
-  } catch (err) {
-    if (this.state.api === true){
-      // if this was an API request, send the error back in a plain response
-      this.body = {error: true, message: String(err)};
-    }else{
-      // this wasn't an API request, show the error page
-      this.app.emit('error', err, this);
-      yield this.render('error', {
-        dump: err
-      });
-    }
-  }
+app.use(function* appUse(next) {
+	try {
+		yield next;
+	} catch (err) {
+		if (this.state.api === true) {
+			// if this was an API request, send the error back in a plain response
+			this.body = {error: true, message: String(err)};
+		} else {
+			// this wasn"t an API request, show the error page
+			this.app.emit("error", err, this);
+			yield this.render("error", {
+				dump: err
+			});
+		}
+	}
 });
 
-require('./routes');
+require("./routes");
 
 console.log(`${config.site.name} is now listening on port ${config.site.port}`);
 app.listen(config.site.port);
 
-process.on('SIGINT', function() {
-  process.exit();
+process.on("SIGINT", () => {
+	process.exit();
 });
--- routes.js
+++ routes.js
@@ -1,72 +1,73 @@
 "use strict";
 
-const config = require('./config.json');
+const config = require("./config.json");
 
-const app = require('./index.js').app;
-const passport = require('./index.js').passport;
-const Router = require('koa-router');
+const app = require("./index.js").app;
+const passport = require("./index.js").passport;
+const Router = require("koa-router");
 
 const routes = new Router();
 
-const main = require('./controllers/main.js');
+const main = require("./controllers/main.js");
 
-const game_hotel = require('./controllers/game_hotel.js');
-const game_bank = require('./controllers/game_bank.js');
-const game_market = require('./controllers/game_market.js');
-const game_airport = require('./controllers/game_airport.js');
-const game_life = require('./controllers/game_life.js');
+const game_hotel = require("./controllers/game_hotel.js");
+const game_bank = require("./controllers/game_bank.js");
+const game_market = require("./controllers/game_market.js");
+const game_airport = require("./controllers/game_airport.js");
+const game_life = require("./controllers/game_life.js");
 
 // routes
-let player = null;
+const player = null;
 
 // app routes
-routes.get('/', main.index);
-routes.get('/account', main.account);
+routes.get("/", main.index);
+routes.get("/account", main.account);
 
 // game routes (these will be replaced by controllers)
-routes.get('/play', game_life.play);
-routes.post('/game/life', game_life.create);
+routes.get("/play", game_life.play);
+routes.post("/game/life", game_life.create);
 
-routes.get('/game/hotel', game_hotel.index);
+routes.get("/game/hotel", game_hotel.index);
 
 // market routes
-routes.get('/game/market', game_market.index);
-routes.post('/game/market/transaction', game_market.transaction);
+routes.get("/game/market", game_market.index);
+routes.post("/game/market/transaction", game_market.transaction);
 
 // airport routes
-routes.get('/game/airport', game_airport.index);
-routes.post('/game/airport/fly', game_airport.fly);
+routes.get("/game/airport", game_airport.index);
+routes.post("/game/airport/fly", game_airport.fly);
 
 // bank routes
-routes.get('/game/bank', game_bank.index);
-routes.get('/game/bank/savings', game_bank.transaction);
-routes.post('/game/bank/savings', game_bank.transaction);
-routes.get('/game/bank/loans', game_bank.lending);
-routes.post('/game/bank/loans', game_bank.lending);
+routes.get("/game/bank", game_bank.index);
+routes.get("/game/bank/savings", game_bank.transaction);
+routes.post("/game/bank/savings", game_bank.transaction);
+routes.get("/game/bank/loans", game_bank.lending);
+routes.post("/game/bank/loans", game_bank.lending);
 
 // for passport
-routes.get('/login', function* (){
-  if (this.isAuthenticated()) {
-    user = this.session.passport.user;
-  }
-  yield this.render('login', {player: player});
+routes.get("/login", function* get() {
+	if (this.isAuthenticated()) {
+		user = this.session.passport.user;
+	}
+	yield this.render("login", {player: player});
 });
 
-routes.get('/logout', function* () {
-  this.logout();
-  this.redirect('/');
+routes.get("/logout", function* get() {
+	this.logout();
+	this.redirect("/");
+	yield this.render("index", {player: player});
 });
 
 // you can add as many strategies as you want
-routes.get('/auth/github',
-  passport.authenticate('github')
+routes.get("/auth/github",
+	passport.authenticate("github")
 );
 
-routes.get('/auth/github/callback',
-  passport.authenticate('github', {
-    successRedirect: '/account',
-    failureRedirect: '/'
-  })
+routes.get("/auth/github/callback",
+	passport.authenticate("github", {
+		successRedirect: "/account",
+		failureRedirect: "/"
+	})
 );
 
 app.use(routes.middleware());

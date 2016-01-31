62c2c13c3bbf22c45f0262922adbb128a9aa15b3
snollygolly <snollygolly@gmail.com>
2016-01-30 23:06:02 -0600

    improved 4th police encounter

--- tests/07e-peaceful-passive-dirty-lucky.js
+++ tests/07e-peaceful-passive-dirty-lucky.js
@@ -34,29 +34,78 @@ describe("Police - Simulating Encounter (Peaceful, Passive, Dirty, Lucky)", () =
 		const policeObj = life.current.police;
 		expect(policeObj.encounter.message.simple).to.be.a("string");
 		expect(policeObj.encounter.message.simple).to.equal(policeJSON.messages.discovery.simple);
+		expect(policeObj.encounter.message.full).to.be.a("string");
+		expect(policeObj.encounter.message.full).to.equal(policeJSON.messages.discovery.full);
 		return done();
 	});
 
 	it("encounter should present choices", (done) => {
 		const policeObj = life.current.police;
-		expect(policeObj.encounter.choices.length).to.be.at.least(1);
+		expect(policeObj.encounter.choices.length).to.be.at.least(4);
 		return done();
 	});
 
-	it("encounter mode should be 'searching' after 'allow_search'", (done) => {
+	it("encounter should accept the 'allow_search' action", (done) => {
 		// simulate the encounter
 		life = simulateAction("permit_search", life);
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.mode).to.be.a("string");
+		return done();
+	});
+
+	it("encounter mode should be 'searching'", (done) => {
 		const policeObj = life.current.police;
 		expect(policeObj.encounter.mode).to.equal("searching");
 		return done();
 	});
 
-	it("encounter should end after 'comply_search'", (done) => {
+	it("encounter reason should be 'consent'", (done) => {
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.reason).to.equal("consent");
+		return done();
+	});
+
+	it("encounter should start 'searching' mode", (done) => {
+		// simulate the encounter
+		life = police.simulateEncounter(life);
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.mode).to.equal("searching");
+		return done();
+	});
+
+	it("encounter should explain what is happening", (done) => {
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.message.simple).to.be.a("string");
+		expect(policeObj.encounter.message.simple).to.equal(policeJSON.messages.search_consent.simple);
+		expect(policeObj.encounter.message.full).to.be.a("string");
+		expect(policeObj.encounter.message.full).to.equal(policeJSON.messages.search_consent.full);
+		return done();
+	});
+
+	it("encounter should present choices", (done) => {
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.choices.length).to.be.at.least(4);
+		return done();
+	});
+
+	it("encounter should accept the 'comply_search' action", (done) => {
 		// simulate the encounter
 		life.current.police.meta = "lucky";
 		life = simulateAction("comply_search", life);
 		const policeObj = life.current.police;
+		expect(policeObj.encounter.mode).to.be.a("string");
+		return done();
+	});
+
+	it("encounter mode should be 'end'", (done) => {
+		const policeObj = life.current.police;
 		expect(policeObj.encounter.mode).to.equal("end");
+		return done();
+	});
+
+	it("encounter reason should be 'search_failure'", (done) => {
+		const policeObj = life.current.police;
+		expect(policeObj.encounter.reason).to.equal("search_failure");
 		return done();
 	});
 });

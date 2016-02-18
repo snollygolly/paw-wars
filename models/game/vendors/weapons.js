a515524c101dd733bf1f7561de8513295c982b7f
snollygolly <snollygolly@gmail.com>
2016-02-17 21:19:49 -0600

    changed vendor models to pass

--- models/game/vendors/weapons.js
+++ models/game/vendors/weapons.js
@@ -4,7 +4,7 @@ const game = require("../../../game.json");
 const common = require("../../../helpers/common");
 const model = require("../../game_life.js");
 
-module.exports.doVendorTransaction = function doVendorTransaction(vendor, life, transaction) {
+module.exports.doVendorTransaction = function doVendorTransaction(life, transaction) {
 	const newLife = JSON.parse(JSON.stringify(life));
 	// start to error check the transactions
 	// first, see what they want to do, and see if the units are available

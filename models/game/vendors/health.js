2ee8624acf54233ac030f8ba8f71852eee0c87cb
snollygolly <snollygolly@gmail.com>
2016-02-29 20:55:02 -0600

    started work on health vendor
    #72

---
+++ models/game/vendors/health.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const game = require("../../../game.json");
+const common = require("../../../helpers/common");
+const model = require("../../game_life.js");
+
+module.exports.doVendorTransaction = function doVendorTransaction(life, transaction) {
+	const newLife = JSON.parse(JSON.stringify(life));
+	// remove sold mod from stock
+	const newMod = newLife.listings.vendors["health"].stock.splice(transaction.index, 1)[0];
+	// take the money from them
+	newLife.current.finance.cash -= newMod.price;
+	// increase the player health
+	newLife.current.health.points += newMod.units;
+	// console.log("* doVendorTransaction:", life);
+	return newLife;
+};
+
+module.exports.generateVendorListings = function generateVendorListings(life) {
+	// generates the prices and units for the vendor
+	const listingObj = {
+		open: true,
+		stock: fillStock(life)
+	};
+
+	return listingObj;
+};
+
+function fillStock(lifeObj) {
+	const stockArr = [];
+	// make some shorthand versions of props
+	const basePrice = game.vendors.base_price * game.vendors.health.pricing.times_base;
+	const increaseRate = game.vendors.health.pricing.increase_rate;
+	// TODO: add code to increase the base price as we aquire more health
+	let lastPrice = basePrice;
+	let i = 0;
+	// add one item for stock count
+	while (i < game.vendors.health.stock) {
+		const stockObj = {
+			units: game.vendors.health.units,
+			name: "health points",
+			price: (lastPrice * increaseRate),
+			meta: null
+		};
+		lastPrice = stockObj.price;
+		stockArr.push(stockObj);
+		i++;
+	}
+	return stockArr;
+}

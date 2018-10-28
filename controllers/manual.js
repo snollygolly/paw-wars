3e9ff86015da4ea36a4533ea32932d827e7287fe
snollygolly <snollygolly@gmail.com>
2018-10-27 23:44:30 -0500

    fixed issue with fs sync reading
    shudder

--- controllers/manual.js
+++ controllers/manual.js
@@ -2,10 +2,18 @@
 
 const config = require("../config.json");
 const game = require("../game.json");
-const fs = require("fs");
+const itemsJSON = require("../models/game/data/items.json");
+const placesJSON = require("../models/game/data/places.json");
+const bluebird = require("bluebird");
+const fs = bluebird.promisifyAll(require("fs"));
+const marked = require("marked");
+marked.setOptions({
+	sanitize: false
+});
+const Handlebars = require("handlebars");
 
 module.exports.index = async(ctx) => {
-	const whitelist = fs.readdirSync("views/manual/");
+	const whitelist = await fs.readdirAsync("views/manual/");
 	let partialName = "index";
 	if (ctx.params.page) {
 		// they passed a page, let's use it
@@ -15,7 +23,20 @@ module.exports.index = async(ctx) => {
 			throw new Error("Page not in whitelist / manualController:index");
 		}
 	}
+	const data = {
+		game: game,
+		config: config,
+		items: itemsJSON,
+		places: placesJSON
+	};
+	const rawFile = await fs.readFileAsync(`views/manual/${partialName}.md`, "utf8");
+	// we have to manually replace escaped quotes because of marked
+	// https://github.com/chjj/marked/issues/269
+	let parsedFile = marked(rawFile);
+	parsedFile = parsedFile.replace(/\\&quot;/g, '"');
+	const template = Handlebars.compile(parsedFile);
+	const final = template(data);
 	await ctx.render("manual/index", {
-		partial: partialName
+		content: final
 	});
 };
--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,17 +1,11 @@
 "use strict";
 
-const fs = require("fs");
 const Handlebars = require("handlebars");
 const hbs = require("koa-hbs");
-const marked = require("marked");
-marked.setOptions({
-	sanitize: false
-});
 const config = require("../config.json");
 const game = require("../game.json");
 const common = require("./common");
 const itemsJSON = require("../models/game/data/items.json");
-const placesJSON = require("../models/game/data/places.json");
 
 hbs.registerHelper("if_eq", function if_eq(a, b, opts) {
 	if (a == b) {
@@ -193,23 +187,6 @@ hbs.registerHelper("get_deal_indication", function stringify(id, price, opts) {
 hbs.registerHelper("get_item_name", function get_item_name(id, opts) {
 	const item = common.getObjFromID(id, itemsJSON);
 	return item.name;
-});
-
-hbs.registerHelper("md_partial", function md_partial(partial, opts) {
-	const data = {
-		game: game,
-		config: config,
-		items: itemsJSON,
-		places: placesJSON
-	};
-	const rawFile = fs.readFileSync(`views/manual/${partial}.md`, "utf8");
-	// we have to manually replace escaped quotes because of marked
-	// https://github.com/chjj/marked/issues/269
-	let parsedFile = marked(rawFile);
-	parsedFile = parsedFile.replace(/\\&quot;/g, '"');
-	const template = Handlebars.compile(parsedFile);
-	const final = template(data);
-	return final;
 });
 
 hbs.registerHelper("get_police_encounter", function log(police) {
